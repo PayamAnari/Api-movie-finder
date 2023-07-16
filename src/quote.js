4812ff74e428c769ca85e7b506c38884a4397952
Payam Anari <anari.p62@gmail.com>
2023-07-16 14:51:57 +0200

    Add a word limit in quote.js

--- src/quote.js
+++ src/quote.js
@@ -3,7 +3,15 @@ async function getQuote() {
     const urlQ = 'https://api.quotable.io/quotes/random?tags=famous-quotes';
     const response = await fetch(urlQ);
     const data = await response.json();
-    return data[0].content;
+    const quote = data[0].content;
+
+    const words = quote.split(' ');
+
+    if (words.length > 17) {
+      return getQuote();
+    }
+
+    return quote;
   } catch (error) {
     console.log(error);
     throw new Error('Unable to get quote');
