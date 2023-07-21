0d79ef47d9783b5df07b85b24aadf7aeda469e16
Payam Anari <anari.p62@gmail.com>
2023-07-21 19:29:25 +0200

    Update movie.js

--- src/views/movie.js
+++ src/views/movie.js
@@ -1,7 +1,7 @@
-let OMDB_KEY = '96082af0';
+const OMDB_KEY = '96082af0';
 
 function getMovie(movieName) {
-  let url = `https://www.omdbapi.com/?t=${movieName}&apikey=${OMDB_KEY}`;
+  const url = `https://www.omdbapi.com/?t=${movieName}&apikey=${OMDB_KEY}`;
 
   return fetch(url)
     .then((resp) => resp.json())
